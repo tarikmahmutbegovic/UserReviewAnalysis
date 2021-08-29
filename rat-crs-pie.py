d8e37e53ff15e0cde6fe8fb525ca3a2338cb032e
Tarik Mahmutbegovic <89174669+tarikmahmutbegovic@users.noreply.github.com>
2021-08-29 16:33:41 -0400

    Add files via upload

---
+++ rat-crs-pie.py
@@ -0,0 +1,84 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+
+data = pandas.read_csv("reviews.csv", parse_dates=["Timestamp"])
+share = data.groupby(["Course Name"])["Rating"].count()
+
+chart_definition = """{
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Number of users per python course'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Sogou Explorer',
+            y: 1.64
+        }, {
+            name: 'Opera',
+            y: 1.6
+        }, {
+            name: 'QQ',
+            y: 1.2
+        }, {
+            name: 'Other',
+            y: 2.61
+        }]
+    }]
+}
+"""
+
+def app():
+    wp = jp.QuasarPage()
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp, text="These data represent a user review analysis for the course in which I learned python3",classes="text-center")
+    hc = jp.HighCharts(a=wp, options = chart_definition)
+    hc_data = [{"name":v1, "y":v2} for v1,v2 in zip(share.index, share)]
+    hc.options.series[0].data = hc_data 
+
+    return wp
+
+jp.justpy(app)
